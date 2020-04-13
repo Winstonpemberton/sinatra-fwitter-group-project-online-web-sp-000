c259bc6fe518ac833274b4f516f5f2a6b6e20d92
Winston Pemberton <winstonpemberton@gmail.com>
2020-04-13 22:24:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_214002) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.string "content"
-  end
+ActiveRecord::Schema.define(version: 2020_04_13_213950) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
